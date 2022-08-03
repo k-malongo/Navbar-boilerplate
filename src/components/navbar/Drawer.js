84618b01a4a9079170368381dd6917de99fb5ae9
Kelvin <vokemalosh32@gmail.com>
2022-08-03 21:17:40 +0300

    done

--- src/components/navbar/Drawer.js
+++ src/components/navbar/Drawer.js
@@ -1,15 +1,25 @@
-import React from 'react'
-import { Drawer } from '@mui/material'
-
- const Drawer = () => {
+import React,{useState} from 'react'
+import { Drawer,List, ListItemButton, ListItemIcon, ListItemText,IconButton } from '@mui/material'
+import MenuIcon from '@mui/icons-material/Menu';
+ const DrawerC = () => {
+    const [openDrawer, setOpenDrawer] = useState(false)
   return (
     <>
-    <Drawer>
-      
-
+    <Drawer open={openDrawer} onClose={()=>setOpenDrawer(false)}>
+      <List>
+        <ListItemButton>
+          <ListItemIcon>
+            <ListItemText>
+              Login  
+            </ListItemText>
+          </ListItemIcon>
+        </ListItemButton>
+      </List>
     </Drawer>
-    
+    <IconButton sx={{color:'white', marginLeft: 'auto'}} onClick={()=> setOpenDrawer(!openDrawer)}>
+    <MenuIcon/>
+    </IconButton>
     </>
   )
 }
-export default Drawer
+export default DrawerC
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,28 +1,60 @@
-import React,{useState} from 'react'
-import {AppBar,Tabs,Tab, Toolbar, Typography,Button} from "@mui/material"
-import ShoppingCartCheckoutIcon from '@mui/icons-material/ShoppingCartCheckout';
+import React, { useState } from "react";
+import {
+  AppBar,
+  Tabs,
+  Tab,
+  Toolbar,
+  useMediaQuery,
+  useTheme,
+  Button,
+  Typography,
+} from "@mui/material";
+import ShoppingCartCheckoutIcon from "@mui/icons-material/ShoppingCartCheckout";
+import DrawerC from "./Drawer";
+const PAGES =["Products","Services","About us","contact us"]
 
 const Navbar = () => {
-  const[value,setValue] = useState()
+  const [value, setValue] = useState();
+  const theme = useTheme();
+  // console.log(theme)
+  const isMatch = useMediaQuery(theme.breakpoints.down("md"));
+  console.log(isMatch);
   return (
     <React.Fragment>
-      <AppBar sx={{background: "#063970"}}>
+      <AppBar sx={{ background: "#063970" }}>
         <Toolbar>
-         <ShoppingCartCheckoutIcon/>
-          <Tabs  textColor="inherit" value={value} onChange={(e, value)=>setValue(value)} indicatorColor="secondary">
-            <Tab label="Products"/>
-            <Tab label="Services"/>
-            <Tab label="contact us"/>
-            <Tab label="About us"/>
+          {isMatch ? (
+            <>
+              <Typography>SHOPEE</Typography>
+              <DrawerC />
+            </>
+          ) : (
+            <>
+              <ShoppingCartCheckoutIcon />
+              <Tabs
+                textColor="inherit"
+                value={value}
+                onChange={(e, value) => setValue(value)}
+                indicatorColor="secondary"
+              >
+                {
+                  PAGES.map((page,index)=>(
+                <Tab key={index} label={page} />
 
-
-          </Tabs>
-          <Button sx={{marginLeft:"auto"}} variant="contained">Login</Button>
-          <Button sx={{marginLeft:"10px"}} variant="contained">SignUp</Button>
+                  ))
+                }
+              </Tabs>
+              <Button sx={{ marginLeft: "auto" }} variant="contained">
+                Login
+              </Button>
+              <Button sx={{ marginLeft: "10px" }} variant="contained">
+                SignUp
+              </Button>
+            </>
+          )}
         </Toolbar>
       </AppBar>
     </React.Fragment>
-
-    )
-}
-export default Navbar
+  );
+};
+export default Navbar;
